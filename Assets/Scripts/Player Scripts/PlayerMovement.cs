8a16cd647ab81934151ea6a60fc29e64e32ace4f
Abyssmouse <abyssmouse@gmail.com>
2022-02-25 10:22:50 +0100

    V0.1.4 Commit
    Added level transition prefab
    Added light radius trigger object
    Removed canvas text for objects, done with components instead
    
    Fixed former text bugs
    Fixed "player movement" sneak bug
    Obligatory script tweaks

--- Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float _speed = 10.0f;
+    [SerializeField] private float _speed = 5.0f;
 
     [Space]
     [SerializeField] private string _horizontalMovementAxisName = "Horizontal";
@@ -16,7 +16,6 @@ public class PlayerMovement : MonoBehaviour
     private Transform _transform;
     private Animator _animator;
     private string _playerTag = "Player";
-    private bool _isSneaking = true;
 
     // Start is called before the first frame update
     void Awake()
@@ -30,31 +29,31 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("left ctrl"))
-        {
-            _animator.SetBool("IsSneaking",_isSneaking);
-        }
+            if (Input.GetKey("left ctrl"))
+            {
+                _animator.SetBool("IsSneaking", true);
+                _speed = 1.0f;
+            }
+            else
+            {
+                _animator.SetBool("IsSneaking", false);
+                _speed = 5.0f;
+            }
 
         _horizontalMovement = Input.GetAxisRaw(_horizontalMovementAxisName);
         _verticalMovement = Input.GetAxisRaw(_verticalMovementAxisName);
         _rigidbody2d.velocity = new Vector2(_horizontalMovement * _speed, _verticalMovement * _speed);
 
-        //IsWalking bool ne radi za vertikalno jos, bug jer dva puta u isto vrijeme može vratit true?
-
-        if (_horizontalMovement != 0.0f)
+        if (_horizontalMovement != 0.0f) ;
         {
             _animator.SetBool("IsWalking", _horizontalMovement != 0.0f);
         }
-
-
     }
 
     private void LateUpdate()
     {
-        Vector3 localScale = _transform.localScale;
+        Vector2 localScale = _transform.localScale;
 
-        // && i operator (shift + 6)
-        // || ili operator (altgr + w)
         if ((_horizontalMovement < 0.0f && localScale.x > 0.0f) ||
             (_horizontalMovement > 0.0f && localScale.x < 0.0f))
             localScale.x *= -1.0f;
