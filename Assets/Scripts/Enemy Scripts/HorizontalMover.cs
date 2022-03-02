424268b37087bc04b2596a3cab04306822ffdf25
Mihajlo Arbanas <valdyr88@gmail.com>
2022-03-02 20:24:28 +0100

    Fix bug guard text flip

--- Assets/Scripts/Enemy Scripts/HorizontalMover.cs
+++ Assets/Scripts/Enemy Scripts/HorizontalMover.cs
@@ -12,17 +12,29 @@ public class HorizontalMover : MonoBehaviour
 	[SerializeField] private float _speed = 2.5f;
 
 	private Rigidbody2D _rigidbody2D;
+	private Transform _textMeshTransform = null;
 
 	private float _xDirection = 1.0f;
 
 	private void Awake()
 	{
 		_rigidbody2D = GetComponent<Rigidbody2D>();
+		
+		for(int i = 0; i < transform.childCount; ++i)
+		{
+			Transform t = transform.GetChild(i);
+			if(t.name == "GuardText")
+			{
+				_textMeshTransform = t;
+				break;
+			}
+		}
 	}
 
 	private void FixedUpdate()
 	{
 		_rigidbody2D.velocity = new Vector2(_xDirection * _speed, _rigidbody2D.velocity.y);
+
 		bool shouldFlip = Physics2D.Linecast(_flipCheckStart.position, _flipCheckEnd.position, _flipLayerMask);
 
 		if(shouldFlip)
@@ -33,6 +45,12 @@ public class HorizontalMover : MonoBehaviour
 			localScale.x *= -1.0f;
 
 			transform.localScale = localScale;
+
+			//Keep the text in right direction
+			if (_textMeshTransform != null)
+			{
+				_textMeshTransform.localScale = localScale;
+			}
 		}
 	}
 
