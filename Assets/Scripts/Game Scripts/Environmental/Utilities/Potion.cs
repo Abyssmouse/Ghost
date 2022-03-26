3dec1b70811abad07a2b530c4eb951eed56032c8
Mihajlo Arbanas <valdyr88@gmail.com>
2022-03-26 11:30:31 +0100

    Moved invisibility and insight logic into PlayerAttributes, and reworked potions to use one script

---
+++ Assets/Scripts/Game Scripts/Environmental/Utilities/Potion.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Potion : MonoBehaviour
+{
+	public enum EPotionType { None, Invisibility, Insight };
+
+	public EPotionType type = EPotionType.None;
+	public float time = 10.0f;
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag != "Player")
+		{
+			return;
+		}
+		else
+		{
+			PlayerAttributes playerAttributes = other.GetComponent<PlayerAttributes>();
+			if (playerAttributes != null)
+			{
+				//switch based on potion type to increase correct timer
+				switch (type)
+				{
+					case EPotionType.None:
+						break;
+
+					case EPotionType.Invisibility:
+						playerAttributes.AddCloakTime(time);
+						break;
+
+					case EPotionType.Insight:
+						playerAttributes.AddEnhancedVisibilityTime(time);
+						break;
+
+					default:
+						break;
+				}
+			}
+
+			SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+			Destroy(spriteRenderer); //destroy sprite
+			Destroy(this); //destroy self
+		}
+	}
+}
--- Assets/Scripts/Player Scripts/PlayerAttributes.cs
+++ Assets/Scripts/Player Scripts/PlayerAttributes.cs
@@ -8,28 +8,59 @@ public class PlayerAttributes : MonoBehaviour
 	public float BasePlayerVisibility { get; } = 0.2f;
 	private float _playerVisibility = 0.2f;
 	private float _additionalVisibility = 0.0f;
-	private bool _isCloaked = false;
+	private float _cloakedTimer = 0.0f;
+	private float _enhancedVisibilityTimer = 0.0f;
 	private GameObject _uivisibility = null;
+	private Animator _playerAnimator = null;
+	private GameObject _mainCamera = null;
 
 	private void Awake()
 	{
 		_playerVisibility = BasePlayerVisibility;
 		_uivisibility = GameObject.FindGameObjectWithTag("UIVisibility");
+		_mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+		GameObject player = GameObject.FindGameObjectWithTag("Player");
+		if(player)
+			_playerAnimator = player.GetComponent<Animator>();
 	}
-
+	
 	public void SetAdditionalVisibiity(float visibility)
 	{
+		//increasing player visibility from BasePlayerVisibility towards 1.0f, based on input visibilty
 		_playerVisibility = Mathf.Lerp(BasePlayerVisibility, 1.0f, visibility);
 		_additionalVisibility = visibility;
 
 		if (_uivisibility != null)
-		{
+		{	
 			_uivisibility.GetComponent<Image>().color = new Color(1.0f, 1.0f, 1.0f, _playerVisibility);
 		}
 	}
 	public float GetPlayerVisibility() { return _playerVisibility; }
 	public float GetAdditionalPlayerVisibility() { return _additionalVisibility; }
 
-	public void SetCloaked(bool cloaked) { _isCloaked = cloaked; }
-	public bool IsCloaked() { return _isCloaked; }
+	public void AddCloakTime(float time) { _cloakedTimer += time; }
+	public bool IsCloaked() { return _cloakedTimer > 0.0f; }
+
+	public void AddEnhancedVisibilityTime(float time) { _enhancedVisibilityTimer += time; }
+	public bool HasEnhancedVisibility() { return _enhancedVisibilityTimer > 0.0f; }
+
+	public void Update()
+	{
+		if (_cloakedTimer > 0.0f)
+		{ 
+			_cloakedTimer -= Time.deltaTime;
+			
+			_playerAnimator.SetBool("IsGhost", IsCloaked()); //IsCloaked() returns _cloakedTimer > 0.0f
+		}
+
+		if (_enhancedVisibilityTimer > 0.0f)
+		{ 
+			_enhancedVisibilityTimer -= Time.deltaTime;
+			
+			if(HasEnhancedVisibility()) //if still has enhanced visibilty then keep orthographicSize to 7
+				Camera.main.orthographicSize = 7;
+			else //else return to default of 5.5
+				Camera.main.orthographicSize = 5.5f;
+		}
+	}
 }
